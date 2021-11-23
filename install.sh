a7c29b7d85d703a1a1a4f0c7ffbc34569d456286
Josh Johanning <joshjohanning@github.com>
2021-11-23 11:17:05 -0600

    adding install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+zshrc() {
+    echo "==========================================================="
+    echo "             cloning zsh-autosuggestions                   "
+    echo "-----------------------------------------------------------"                    
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "==========================================================="
+    echo "             cloning zsh-syntax-highlighting                   "
+    echo "-----------------------------------------------------------"                    
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    echo "==========================================================="
+    echo "                  Import zshrc                             "
+    echo "-----------------------------------------------------------"
+    cat .zshrc > $HOME/.zshrc
+}
+
+zshrc
