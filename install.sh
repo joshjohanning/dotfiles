6a900b720870d184176e65355410d70cbb4af2c6
Josh Johanning <joshjohanning@github.com>
2022-01-04 09:52:00 -0600

    adding extra line breaks

--- install.sh
+++ install.sh
@@ -26,8 +26,10 @@ zshrc() {
 zshrc
 
 # these need to be after zshrc
+echo "" >> ~/.zshrc
 echo "# remove ls highlight color" >> ~/.zshrc
 echo "LS_COLORS+=':ow=01;33'" >> ~/.zshrc
 
+echo "" >> ~/.zshrc
 echo "# set time zone" >> ~/.zshrc
 echo "export TZ=America/Chicago" >> ~/.zshrc
